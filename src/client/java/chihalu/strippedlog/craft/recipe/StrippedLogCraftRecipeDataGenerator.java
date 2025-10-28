9638c8f2cf9cc8538d67950c509011485cd5cb9a
Chihalu <m.chihiro0314@gmail.com>
2025-10-29 01:51:23 +0900

    first commit

---
+++ src/client/java/chihalu/strippedlog/craft/recipe/StrippedLogCraftRecipeDataGenerator.java
@@ -0,0 +1,11 @@
+package chihalu.strippedlog.craft.recipe;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class StrippedLogCraftRecipeDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
